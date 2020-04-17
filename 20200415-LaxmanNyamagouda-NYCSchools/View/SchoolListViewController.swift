4f0c74406b67e579a165bc632914c39b728b880f
Laxman Nyamagouda <laxman.nyamagouda@gmail.com>
2020-04-16 23:35:02 -0400

    Remove space

--- 20200415-LaxmanNyamagouda-NYCSchools/View/SchoolListViewController.swift
+++ 20200415-LaxmanNyamagouda-NYCSchools/View/SchoolListViewController.swift
@@ -23,7 +23,6 @@ class SchoolListViewController: UIViewController {
     
     var didSelectSchool: ((_ index: Int) -> ())?
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
